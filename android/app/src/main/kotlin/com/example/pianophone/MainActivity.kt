ab17ecd4f09624a5a8ada96db04a75aca209c86a
SBilaal <mbilaalme@gmail.com>
2022-02-09 22:03:03 +0100

    Pianophone Starter Project

---
+++ android/app/src/main/kotlin/com/example/pianophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pianophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PianophoneApp());
+
+class PianophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+          ),
+        ),
+      ),
+    );
+  }
+}
